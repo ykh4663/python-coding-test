909385c5e0aef3083dc5a1d831baa76878f280cf
ykh4663 <superho7@gmail.com>
2026-02-16 18:07:58 +0900

    나머지 한 점 문제

---
+++ java/programmers_1878.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[][] v) {
+        int [] answer = new int[2];
+        Map<Integer,Integer>rows = new HashMap<>();
+        Map<Integer,Integer>cols = new HashMap<>();
+        for(int i = 0; i < 3; i++)
+        {
+            rows.put(v[i][0], rows.getOrDefault(v[i][0], 0)+1);
+            cols.put(v[i][1], cols.getOrDefault(v[i][1], 0)+1);
+        }
+        
+        for(Map.Entry<Integer, Integer> r : rows.entrySet()){
+            int x = r.getKey();
+            int cnt = r.getValue();
+            if(cnt == 1){
+                answer[0] = x;
+            }
+        }
+        
+        for(Map.Entry<Integer, Integer> c : cols.entrySet()){
+            int y = c.getKey();
+            int cnt = c.getValue();
+            if(cnt == 1){
+                answer[1] = y;
+            }
+        }
+        
+
+        
+
+        return answer;
+    }
+}
